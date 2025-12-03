772e876fc147cb0dc92e193ccadaab7728189b5a
Java-407 <azimovjavohir3442@gmail.com>
2025-12-03 12:23:56 +0500

    Update controller.py

--- Javohir/controller.py
+++ Javohir/controller.py
@@ -1,59 +1,31 @@
 from builder import SmartCityBuilder
 from logger import Logger
-from proxy import SecurityProxy
 from adapter import WeatherAdapter, ExternalWeatherAPI
 
-
-class SmartCityController:
-    """
-    Facade — barcha modullar bilan ishlashni soddalashtiradi.
-    Singleton — tizimda faqat bitta markaziy boshqaruvchi bo‘ladi.
-    """
-    __instance = None
+class SystemController:
+    _instance = None
 
     def __init__(self):
+        if SystemController._instance is not None:
+            raise Exception("Singleton!")
         self.log = Logger.get_instance()
-
-        # Builder orqali modullarni quramiz
         self.city = SmartCityBuilder()\
             .add_lighting()\
             .add_transport()\
             .add_security()\
             .add_energy()\
             .build()
-
-        self.log.log("SmartCity tizimi ishga tushirildi!")
+        self.weather = WeatherAdapter()
+        self.log.log("SmartCityController ishga tushdi!")
 
     @staticmethod
     def get_instance():
-        if SmartCityController.__instance is None:
-            SmartCityController.__instance = SmartCityController()
-        return SmartCityController.__instance
+        if SystemController._instance is None:
+            SystemController._instance = SystemController()
+        return SystemController._instance
 
-    def run(self):
-        """
-        Konsol interfeysi
-        """
-        while True:
-            print("""
-=== SMART CITY ===
-1) Ko‘cha yoritishi
-2) Transport
-3) Xavfsizlik
-4) Energiya
-5) Ob-havo (Adapter)
-0) Chiqish
-""")
-            c = input("Tanlang: ")
-
-            if c == "0": break
-            if c == "1": self.city["lighting"].turn_on()
-            if c == "2": self.city["transport"].add_bus()
-            if c == "3":
-                parol = input("Parol: ")
-                SecurityProxy(self.city["security"]).monitor(parol)
-            if c == "4":
-                self.city["energy"].save(3)
-                print(self.city["energy"].report())
-            if c == "5":
-                print(WeatherAdapter(ExternalWeatherAPI()).city_temp())
+    def start_city(self):
+        print("=== SHAHAR TIZIMLARI ISHGA TUSHIRILDI ===")
+        self.city["lighting"].turn_on()
+        self.city["transport"].show_routes()
+        self.city["security"].scan()
