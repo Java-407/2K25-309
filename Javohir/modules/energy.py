528c3938c060dd4eb4aa174334fabb55c442222a
Java-407 <azimovjavohir3442@gmail.com>
2025-12-03 12:26:46 +0500

    Update energy.py

--- Javohir/modules/energy.py
+++ Javohir/modules/energy.py
@@ -1,16 +1,21 @@
 from logger import Logger
 
 class EnergySystem:
-    """
-    Energiya tejash statistikasini yuritish
-    """
     def __init__(self):
         self.savings = 0
         self.log = Logger.get_instance()
 
     def save(self, amount):
         self.savings += amount
-        self.log.log(f"Energiya tejandi: {amount} kW")
+        self.log.log(f"Energiya tejaldi: {amount} kW")
 
     def report(self):
         return f"Jami tejangan energiya: {self.savings} kW"
+
+class EnergyMonitorDecorator:
+    def __init__(self, energy_system):
+        self.energy_system = energy_system
+
+    def report(self):
+        base = self.energy_system.report()
+        return f"[Monitoring] {base} | Tizim barqaror"
