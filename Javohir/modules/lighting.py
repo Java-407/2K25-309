85015e95da4d81781bbce28f08a38a31b851c53a
Java-407 <azimovjavohir3442@gmail.com>
2025-12-03 12:25:55 +0500

    Update lighting.py

--- Javohir/modules/lighting.py
+++ Javohir/modules/lighting.py
@@ -1,17 +1,6 @@
-from logger import Logger
-
 class LightingSystem:
-    """
-    Shahar ko‘cha chiroqlarini boshqarish
-    """
-    def __init__(self):
-        self.state = False
-        self.log = Logger.get_instance()
-
     def turn_on(self):
-        self.state = True
-        self.log.log("Ko‘cha chiroqlari yoqildi.")
+        print("Ko‘cha chiroqlari YOQILDI")
 
     def turn_off(self):
-        self.state = False
-        self.log.log("Ko‘cha chiroqlari o‘chirildi.")
+        print("Ko‘cha chiroqlari O‘CHIRILDI")
